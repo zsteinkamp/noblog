a44c00c45918d3de29599fd55237506a260162ac
Zack Steinkamp <zs@thenobot.org>
2013-06-15 18:34:00 -0700

    initial commit, bare rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevSteinkampUs::Application.config.secret_token = '6638167bd629010f677de468cf008e8fbe12ef515b8494e87ab608e4887727a81065aec99970fb83ef2529997682ee4e0735eb8d44ccf124dca3420f80079cd6'
