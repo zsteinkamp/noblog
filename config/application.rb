532b05372d735cbdacefe296762e12a360c9925a
Zack Steinkamp <zs@thenobot.org>
2015-10-07 16:11:17 -0700

    precompile ekko-lightbox

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module Noblog
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['ekko-lightbox.css', 'ekko-lightbox-dark.css', 'ekko-lightbox.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
