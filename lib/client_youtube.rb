3477488675e51d21c5250370631c78e6ca6f795d
Zack Steinkamp <zs@thenobot.org>
2015-10-07 16:44:04 -0700

    fix youtube

--- lib/client_youtube.rb
+++ lib/client_youtube.rb
@@ -8,18 +8,16 @@ class ClientYoutube
   end
 
   def get_videos
-    endpoint = "http://gdata.youtube.com/feeds/base/users/#{@username}/uploads?alt=rss&v=2"
+    endpoint = "https://www.youtube.com/feeds/videos.xml?user=#{@username}"
     response = REXML::Document.new(open(endpoint).read)
 
-    response.elements.collect("rss/channel/item") do |item|
-      desc_doc = REXML::Document.new(item.elements["description"].text.gsub(/(<(img|br)[^>]*)>/, "$1/>"))
-      bare_desc = item.elements["description"].text.match(/<span>([^<]*)<\/span>/)[1]
+    response.elements.collect("feed/entry") do |item|
       {
-        :pub_date => item.elements["pubDate"].text,
+        :pub_date => item.elements["published"].text,
         :title => item.elements["title"].text,
-        :link => "#{item.elements["link"].text}&hd=1",
-        :description => bare_desc,
-        :id => item.elements["link"].text.match(/v=([^&]+)/)[1],
+        :link => "https://www.youtube.com/watch?v=#{item.elements["yt:videoId"].text}&hd=1",
+        :description => item.elements["media:group"].elements["media:description"].text,
+        :id => item.elements["yt:videoId"].text
       }
     end
   end
