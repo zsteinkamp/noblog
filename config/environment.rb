5286574cf17d9c3ab925860ea93eadc23d5610fb
Zack Steinkamp <zs@thenobot.org>
2017-11-01 23:19:17 -0700

    encoding

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,8 @@
 require File.expand_path('../application', __FILE__)
 require 'rails_rinku'
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Initialize the rails application
 Noblog::Application.initialize!
